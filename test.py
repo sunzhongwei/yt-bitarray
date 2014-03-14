1f3b5e55c987f54d4535747a13fc6b31870b2f64
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2014-03-14 19:45:26 +0800

    first version

---
+++ test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------
+# DESCRIPTION
+# ===========
+#
+# ----------------------------------------
+
+# build-in, 3rd party and my modules
+import unittest
+from yt_bitarray import YTBitArray
+
+
+class TestYTBitArray(unittest.TestCase):
+    def setUp(self):
+        available_choices = ["apple", "banana", "mongo", "orange", "pear"]
+        self.bit_array = YTBitArray(available_choices)
+
+    def tearDown(self):
+        pass
+
+    def test_convert_choices_to_int(self):
+        selected_choices = ["apple", "orange"]
+        value = self.bit_array.convert_choices_to_int(selected_choices)
+
+        expected_value = int("10010", 2)
+        self.assertEqual(expected_value, value)
+
+    def test_for_fail(self):
+        value = int("10010", 2)
+        selected_choices = self.bit_array.convert_int_to_choices(value)
+
+        expected_value = ["apple", "orange"]
+        for item in expected_value:
+            self.assertIn(item, selected_choices)
+
+
+if '__main__' == __name__:
+    unittest.main()
+
---
+++ yt_bitarray.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1"
+__author__ = "Zhongwei Sun (zhongwei.sun2008@gmail.com)"
+
+# ----------------------------------------
+# Just say "Hello world!".
+# ----------------------------------------
+
+# build-in, 3rd party and my modules
+
+class YTBitArray(object):
+    def __init__(self, available_choices):
+        self.available_choices = available_choices
+        self.choices_count = len(available_choices)
+
+    def convert_choices_to_int(self, choices):
+        value = 0
+        for choice in choices:
+            try:
+                index = self.available_choices.index(choice) + 1
+            except ValueError:
+                pass
+            value = value | (1 << index)
+
+        return value
+
+    def convert_int_to_choices(self, value):
+        choices = []
+        binary_string = bin(value)[2:]
+        for index, selected in enumerate(binary_string[::-1]):
+            if selected == "1":
+                choices.append(
+                        self.available_choices[self.choices_count - index - 1])
+
+        return choices
+
